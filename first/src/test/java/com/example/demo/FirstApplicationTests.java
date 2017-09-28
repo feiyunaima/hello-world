d417ec4fc0833b207f4ef0ffa4993b6e9586a79c
feiyunaima <289367567@qq.com>
2017-09-28 17:42:24 +0800

    改一下看
    看1

--- first/src/test/java/com/example/demo/FirstApplicationTests.java
+++ first/src/test/java/com/example/demo/FirstApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FirstApplicationTests {
-
+//改一下看有用没？
 	@Test
 	public void contextLoads() {
 	}
